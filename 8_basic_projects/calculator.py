9635cdd30a41c6980a68713db7ceafbbd6a5ccc7
Madani ouahmane <100386993+Madanio@users.noreply.github.com>
2024-11-18 23:19:58 +0100

    commit

--- 8_basic_projects/calculator.py
+++ 8_basic_projects/calculator.py
@@ -21,3 +21,18 @@ def devision(a,b) :
         x = a / b
         print ("le resultat de l'opperation et : ")
         print(f" {a} / {b} = {x}")
+
+def choixdoperation(o, a, b):
+    if o == 1:
+        addition(a, b)
+    elif o == 2:
+        soustraction(a, b)
+    elif o == 3 :
+        multiplication(a, b)
+    elif o == 4 :
+        devision(a, b)
+    elif o == 5:
+        print("Au revoir !")
+        return
+    else :
+        print("opperation introuvable")
