152b6b8af1cf61280a9dc3839aa613e87cf72367
Madani ouahmane <100386993+Madanio@users.noreply.github.com>
2024-11-18 23:19:13 +0100

    commit

--- 8_basic_projects/calculator.py
+++ 8_basic_projects/calculator.py
@@ -8,3 +8,16 @@ def soustraction(a,b) :
     x = a - b
     print ("le resultat de l'opperation et : ")
     print(f" {a} - {b} = {x}")
+
+def multiplication(a,b) : 
+    x = a * b
+    print ("le resultat de l'opperation et : ")
+    print(f" {a} x {b} = {x}")
+
+def devision(a,b) : 
+    if b == 0 : 
+        print("Error : devision par zero ")
+    else :
+        x = a / b
+        print ("le resultat de l'opperation et : ")
+        print(f" {a} / {b} = {x}")
