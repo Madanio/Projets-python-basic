47a51bbcb2b2b9880ec6a6284011f7f2acc1ffe5
Madani ouahmane <100386993+Madanio@users.noreply.github.com>
2024-11-18 23:12:18 +0100

    adding project base

--- 8_basic_projects/calculator.py
+++ 8_basic_projects/calculator.py
@@ -1,1 +1,10 @@
 # this is the project one 
+def addition(a,b) : 
+    x = a + b
+    print ("le resultat de l'opperation et : ")
+    print(f" {a} + {b} = {x}")
+
+def soustraction(a,b) : 
+    x = a - b
+    print ("le resultat de l'opperation et : ")
+    print(f" {a} - {b} = {x}")
